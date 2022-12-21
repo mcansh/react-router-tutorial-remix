f412964e57e0a1caf79c9c93b95cf5f96694fabb
Logan McAnsh <logan@mcan.sh>
2022-12-20 23:16:14 -0500

    👉 Create an error page component
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ app/error-page.tsx
@@ -0,0 +1,17 @@
+interface ErrorResponse extends Error {
+  statusText?: string;
+}
+
+export default function ErrorPage({ error }: { error: ErrorResponse }) {
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
