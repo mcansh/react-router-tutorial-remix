46a40a5d4de18cc992b2df8a5a940d83ed83782a
Logan McAnsh <logan@mcan.sh>
2022-12-20 23:20:38 -0500

    chore: render full document in `<ErrorPage>`
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/error-page.tsx
+++ app/error-page.tsx
@@ -1,3 +1,5 @@
+import { Meta, Links } from "@remix-run/react";
+
 interface ErrorResponse extends Error {
   statusText?: string;
 }
@@ -6,12 +8,22 @@ export default function ErrorPage({ error }: { error: ErrorResponse }) {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="root">
+          <div id="error-page">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+              <i>{error.statusText || error.message}</i>
+            </p>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
