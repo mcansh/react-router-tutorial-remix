c9645bfb00876fbaa8e3805db36245a388d2b7fe
Logan McAnsh <logan@mcan.sh>
2022-12-20 23:11:24 -0500

    👉 Create the root layout component
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,12 +7,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import appStylesHref from "./styles/index.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: appStylesHref }];
+};
 
 export default function App() {
   return (
@@ -22,7 +27,38 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <div id="root">
+          <div id="sidebar">
+            <h1>React Router Contacts</h1>
+            <div>
+              <form id="search-form" role="search">
+                <input
+                  id="q"
+                  aria-label="Search contacts"
+                  placeholder="Search"
+                  type="search"
+                  name="q"
+                />
+                <div id="search-spinner" aria-hidden hidden={true} />
+                <div className="sr-only" aria-live="polite"></div>
+              </form>
+              <form method="post">
+                <button type="submit">New</button>
+              </form>
+            </div>
+            <nav>
+              <ul>
+                <li>
+                  <a href={`contacts/1`}>Your Name</a>
+                </li>
+                <li>
+                  <a href={`contacts/2`}>Your Friend</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div id="detail"></div>
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
